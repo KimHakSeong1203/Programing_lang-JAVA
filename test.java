84ecc0df82fe10dfd40f29cd9a37e90aa0e55fcc
KimHakSeong1203 <81869289+KimHakSeong1203@users.noreply.github.com>
2021-06-22 14:37:50 +0900

    JAVA_Practice(1_2)
    System 안에 out 안에 print 관련 메소드 3가지를 다루어 봤습니다. 각 메소드 마다 특이점을 가지고 있으니 유의하시길 바랍니다.

---
+++ test.java
@@ -0,0 +1,11 @@
+package test2;
+
+public class test {
+
+	public static void main(String[] args) {
+		System.out.print("기본 출력, ");
+		System.out.println("출력 후 줄 바뀜");
+		System.out.printf("%d + %d = %d\n", 1, 2, 1+2);
+	}
+
+}
